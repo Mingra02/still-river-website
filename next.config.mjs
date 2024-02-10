e89e5692a2b59d07bfaa4f4d7915911c9ba408ff
Michael Ingram <1r0nm4g3@gmail.com>
2024-02-10 13:57:55 -0500

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const pathPrefix =
+  process.env.LOCATION === "GH_PAGES" ? "/still-river-website/" : "";
+
 const nextConfig = {
   output: "export",
+  distDir: "./out",
   images: {
     unoptimized: true,
   },
+  assetPrefix: pathPrefix,
 };
 
 export default nextConfig;
